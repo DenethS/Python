24a6209ccf0223627360fae7a7dc5443a4d120a0
DenethS <robinlana88@gmail.com>
2024-04-02 12:56:35 +1300

    Triangle area

---
+++ Triangle area.py
@@ -0,0 +1,6 @@
+#Base and height values
+base = float(input("Base length: "))
+height = float(input("Height: "))
+#Calculating the area of the triangle
+area = 0.5 * base * height
+print(f"The area of the triangle is: {area}")
