6fff9b3563d75a92aaf818f09a4909515a0e10b6
DenethS <robinlana88@gmail.com>
2024-04-02 12:47:58 +1300

    Division

--- Addition and division.py
+++ Addition and division.py
@@ -3,3 +3,11 @@ num1 = float(input("First number: "))
 num2 = float(input("Second number: "))
 number_sum = num1 + num2
 print(f"sum: {num1} + {num2} = {number_sum}")
+
+#Division
+num4 = float(input("Second number: "))
+if num4 == 0:
+    print("Cannot divide by 0")
+else:
+    number_div = num3 / num4
+    print(f"division: {num3} / {num4} = {number_div}")
