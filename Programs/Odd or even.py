996d96294a13e6026a0acf53ed98aa49cc2af116
DenethS <robinlana88@gmail.com>
2024-04-02 15:02:04 +1300

    Odd or even

---
+++ Programs/Odd or even.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+
+#if x % 2 == 0 checks if a number is even
+#x % 2 is 1 when the number is odd, and 0 when it is even
+
+if num%2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
