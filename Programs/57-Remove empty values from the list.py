272436b8a9c4a37b00a6e21161c9d921661bc80e
DenethS <robinlana88@gmail.com>
2024-04-08 11:32:36 +1200

    Removing empty lists

---
+++ Programs/57-Remove empty values from the list.py
@@ -0,0 +1,5 @@
+list_of_lists = [[1, 2, 3], [], [4, 5], [], [6, 7, 8], []]
+
+#Using a list comprehension to remove empty lists
+filtered_list = [i for i in list_of_lists if i]
+print("List after removing empty values: ", filtered_list)
