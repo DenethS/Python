cf703896a12016cb71d1e4347a8924c06b687778
DenethS <robinlana88@gmail.com>
2024-04-23 16:30:11 +1200

    Summing up budgets

---
+++ Programs/116-Returning the sum of people's budgets.py
@@ -0,0 +1,6 @@
+# Example
+# get_budgets([
+# { 'name': 'John', 'age': 21, 'budget': 23000 },
+# { 'name': 'Steve', 'age': 32, 'budget': 40000 },
+# { 'name': 'Martin', 'age': 16, 'budget': 2700 }
+# ]) ➞ 65700
