0f3df079acc22cebf54fc94af3ce27a34f8cb433
DenethS <robinlana88@gmail.com>
2024-04-24 11:45:10 +1200

    Multiplication

---
+++ Programs/Return the product of the numbers.py
@@ -0,0 +1,20 @@
+# Examples
+# multiply_nums("2, 3") ➞ 6
+# multiply_nums("1, 2, 3, 4") ➞ 24
+# multiply_nums("54, 75, 453, 0") ➞ 0
+
+def multiply_nums(nums_str):
+    # Split the input string by comma and space, then convert to integer
+    nums = [int(num) for num in nums_str.split(", ")]
+
+    # Initialize the result with 1
+    result = 1
+
+    # Multiply all the numbers together
+    for num in nums:
+        result *= num
+
+    return result
+
+print(multiply_nums("2, 3"))
+print(multiply_nums("54, 75, 453, 0"))
