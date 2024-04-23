0c4b1ad4e1ea5f1c579a051a81d59347ae50e0b7
DenethS <robinlana88@gmail.com>
2024-04-23 15:54:46 +1200

    Finding even numbers

---
+++ Programs/109-Find all even numbers from 1 to given number.py
@@ -0,0 +1,10 @@
+# Examples
+# find_even_nums(8) ➞ [2, 4, 6, 8]
+# find_even_nums(4) ➞ [2, 4]
+# find_even_nums(2) ➞ [2]
+
+def find_even_nums(num):
+    # Use a list comprehension to generate even numbers from 1 to num
+    return [x for x in range(1, num + 1) if x % 2 == 0]
+
+print(find_even_nums(98))
