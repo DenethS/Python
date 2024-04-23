0a4eca6728c7eba5531f006a5a02eb69ad7bd6bf
DenethS <robinlana88@gmail.com>
2024-04-23 16:34:49 +1200

    Alphabet soup

--- Programs/116-Returning the sum of people's budgets.py
+++ Programs/116-Returning the sum of people's budgets.py
@@ -4,3 +4,21 @@
 # { 'name': 'Steve', 'age': 32, 'budget': 40000 },
 # { 'name': 'Martin', 'age': 16, 'budget': 2700 }
 # ]) ➞ 65700
+
+def get_budgets(lst):
+    total_budget = sum(person["budget"] for person in lst)
+    return total_budget
+
+# Test cases
+budgets1 = [
+    {'name': 'John', 'age': 21, 'budget': 23000},
+    {'name': 'Steve', 'age': 32, 'budget': 40000}
+]
+
+budgets2 = [
+    {'name': 'John', 'age': 21, 'budget': 29000},
+    {'name': 'Steve', 'age': 32, 'budget': 32000}
+]
+
+print(get_budgets(budgets1))
+print(get_budgets(budgets2))
