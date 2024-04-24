aaf263eb383a95c4d946c43eb57da1ec09c429f7
DenethS <robinlana88@gmail.com>
2024-04-24 12:06:58 +1200

    Return integer with condition

---
+++ Programs/130-Take an integer and return a list from 1 to the given number.py
@@ -0,0 +1,20 @@
+# Create a function that takes an integer and returns a list from 1 to the given number, where:
+# 1. If the number can be divided evenly by 4, amplify it by 10 (i.e. return 10 times the number).
+# 2. If the number cannot be divided evenly by 4, simply return the number.
+
+# Examples
+# amplify(4) ➞ [1, 2, 3, 40]
+# amplify(3) ➞ [1, 2, 3]
+
+# Notes
+# The given integer will always be equal to or greater than 1.
+# Include the number (see example above).
+# To perform this problem with its intended purpose, try doing it with list comprehensions. If that's too difficult, just solve the challenge any way you can.
+
+def amplify(num):
+    # Use a list comprehension to generate the list
+    return [n * 10 if n % 4 == 0 else n for n in range(1, num + 1)]
+
+print(amplify(4))
+print(amplify(3))
+print(amplify(25))
